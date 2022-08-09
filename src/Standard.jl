c9c93738a7f5109e1516fd5033b347b424cd7344
Rratic <Rratic@163.com>
2022-08-09 11:16:46 +0800

    fix known

--- src/Standard.jl
+++ src/Standard.jl
@@ -2,6 +2,7 @@ module LL_Standard
 include("types.jl")
 include("shortcuts.jl")
 
+Level(initializer)=Level(initializer, st -> isa(st.grids[st.x, st.y], Flag))
 include("levels/1.jl")
 include("levels/2.jl")
 include("levels/3.jl")
@@ -10,20 +11,18 @@ include("levels/5.jl")
 include("levels/6.jl")
 
 function init(st)
-	cd(joinpath(@__DIR__, "../assets")) do
-		load_imgsources(st, "images")
+	cd(joinpath(@__DIR__, "../assets/images")) do
+		load_imgsources(st)
 	end
 end
 
-const levels = Dict{String, Dict{String, Level}}(
-	"基础" => Dict{String, Level}(
-		"简介" => lv_intro,
-		"条件的使用" => lv_state,
-		"循环的使用" => lv_loop,
-		"异常处理" => lv_error,
-		"函数定义" => lv_func,
-		"数组使用" => lv_vector,
-	)
-)
+const levels = [
+	"简介" => lv_intro,
+	"条件的使用" => lv_state,
+	"循环的使用" => lv_loop,
+	"异常处理" => lv_error,
+	"函数定义" => lv_func,
+	"数组使用" => lv_vector,
+]
 
 end # module
